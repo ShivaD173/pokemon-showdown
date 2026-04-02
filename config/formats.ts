2581bd11c3b7141ec9559ed27eb700495006cdc8
Shiva Devarajan <arcubedgs@gmail.com>
2026-04-01 23:58:26 -0400

    dpp su sleep clause

--- config/formats.ts
+++ config/formats.ts
@@ -276,7 +276,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		name: "[Gen 4] SU",
 		mod: 'gen4',
 		searchShow: true,
-		ruleset: ['[Gen 4] ZU'],
+		ruleset: ['[Gen 4] ZU', 'Sleep Moves Clause', '!Sleep Clause Mod'],
 		banlist: ['ZU', 'SUBL', 'Baton Pass'],
 	},
 	{
